1d9b90f06b1375f874864df37f95530279bac18c
celtare21 <celtare21@gmail.com>
2020-05-02 16:43:13 +0200

    ginkgo: This is Artemis
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -50,7 +50,7 @@ BOARD_KERNEL_SEPARATED_DTBO := true
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_SOURCE := kernel/xiaomi/ginkgo
-TARGET_KERNEL_CONFIG := vendor/ginkgo-perf_defconfig
+TARGET_KERNEL_CONFIG := artemis_ginkgo_defconfig
 
 # Enable stats logging in LMKD
 TARGET_LMKD_STATS_LOG := true
